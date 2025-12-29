69a7af0f41451e6df3afc0d81a8115c2ea67cfff
Ayatullah <ayatullah284@gmail.com>
2025-12-29 08:25:21 +0600

    Hoisting commit

---
+++ Hoisting.js
@@ -0,0 +1,12 @@
+console.log('name is ', nam);
+console.log('home: ', person)
+var nam;
+nam = 'tom'
+var person;
+person = {
+    id: 10,
+    roll: 20,
+    home: 'ghatail'
+}
+console.log('name is ', nam)
+console.log('home: ', person.home)
